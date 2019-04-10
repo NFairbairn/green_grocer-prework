0d3b5eb2206bb3c2f7e8b9d1c66b63e0ed1bbd2c
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-10 20:55:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -11,32 +11,33 @@ def consolidate_cart(cart)
       end
     end
   end
+end
 
-  def apply_coupons(cart, coupons)
-    coupons.each do |coupon|
-      name = coupon[:item]
-      if cart[name] && cart[name][:count] >= coupon[:num]
-        if cart["#{name} W/COUPON"]
-          cart["#{name} W/COUPON"][:count] += 1
-        else
-          cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
-          cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
-        end
-        cart[name][:count] -= coupon[:num]
-      end
+def apply_coupons(cart, coupons)
+coupons.each do |coupon|
+  name = coupon[:item]
+  if cart[name] && cart[name][:count] >= coupon[:num]
+    if cart["#{name} W/COUPON"]
+      cart["#{name} W/COUPON"][:count] += 1
+    else
+      cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+      cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
     end
-    cart
+    cart[name][:count] -= coupon[:num]
   end
+end
+cart
+end
 
-  def apply_clearance(cart)
-    cart.each do |name, properties|
-      if properties[:clearance]
-        updated_price = properties[:price] * 0.80
-        properties[:price] = updated_price.round(2)
-      end
-    end
-    cart
+def apply_clearance(cart)
+cart.each do |name, properties|
+  if properties[:clearance]
+    updated_price = properties[:price] * 0.80
+    properties[:price] = updated_price.round(2)
   end
+end
+cart
+end
 
   def checkout(cart, coupons)
     consolidated_cart = consolidate_cart(cart)
