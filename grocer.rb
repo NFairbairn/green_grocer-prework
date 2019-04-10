5ba4bf29eadc6b7fff405fb9e0cfa9e857ecc912
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-10 20:53:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,9 +12,21 @@ def consolidate_cart(cart)
     end
   end
 
-def apply_coupons(cart, coupons)
-  # code here
-end
+  def apply_coupons(cart, coupons)
+    coupons.each do |coupon|
+      name = coupon[:item]
+      if cart[name] && cart[name][:count] >= coupon[:num]
+        if cart["#{name} W/COUPON"]
+          cart["#{name} W/COUPON"][:count] += 1
+        else
+          cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+          cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+        end
+        cart[name][:count] -= coupon[:num]
+      end
+    end
+    cart
+  end
 
 def apply_clearance(cart)
   # code here
